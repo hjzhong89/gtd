c007524394bd3b2bc80581187507ca84f994139f
hjzhong89 <dev8>
2020-11-18 08:03:49 -0800

    Hovering

--- src/components/PointGroup.vue
+++ src/components/PointGroup.vue
@@ -2,8 +2,10 @@
   <g class="point-group" :id="groupName">
     <circle v-for="(circle, i) in circles"
             :key="i"
-            class="point"
+            class="point grow"
             v-bind="circle"
+            @mouseover="onmouseover"
+            @mouseout="onmouseout"
     ></circle>
   </g>
 </template>
@@ -25,7 +27,7 @@ export default {
     },
     margin: {
       type: Object,
-      default: () => ({ top: 0, bottom: 0, left: 0, right: 0 })
+      default: () => ({top: 0, bottom: 0, left: 0, right: 0})
     },
     points: {
       type: Array,
@@ -36,21 +38,31 @@ export default {
     circles() {
       const projection = d3.geoMercator();
       projection.scale(this.canvasWidth / (2 * Math.PI))
-                .translate([
-                             (this.canvasWidth + this.margin.left + this.margin.right) / 2,
-                             (this.canvasHeight + this.margin.top + this.margin.bottom) / 2,
-                           ]);
+        .translate([
+          (this.canvasWidth + this.margin.left + this.margin.right) / 2,
+          (this.canvasHeight + this.margin.top + this.margin.bottom) / 2,
+        ]);
 
       return this.points.map(p => {
-        const [ cx, cy ] = projection([ p.longitude, p.latitude ])
-        return {
-          ...p,
-          cx,
-          cy,
-
-        }
+        const [cx, cy] = projection([p['data-longitude'], p['data-latitude']])
+        return {...p, cx, cy}
       })
     },
   },
+  methods: {
+    onmouseover(e) {
+      const point = d3.select(e.target);
+      point.attr('r', point.attr('r') * 1.5)
+    },
+    onmouseout(e) {
+      const point = d3.select(e.target);
+      point.attr('r', point.attr('r') / 1.5)
+    }
+  },
 }
 </script>
+<style lang="scss">
+.point {
+  cursor: pointer;
+}
+</style>
